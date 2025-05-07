8fac897b886a4f0a8cec769cc3310ce41f0f04a7
LinhAn <157439726+LinhAn04@users.noreply.github.com>
2025-05-07 22:30:38 +0700

    Exception quản lý phim

--- HeThongQuanLyRapPhim/src/main/java/hcmute/edu/vn/HeThongQuanLyRapPhim/repository/MovieRepository.java
+++ HeThongQuanLyRapPhim/src/main/java/hcmute/edu/vn/HeThongQuanLyRapPhim/repository/MovieRepository.java
@@ -4,8 +4,13 @@ import hcmute.edu.vn.HeThongQuanLyRapPhim.model.Phim;
 import hcmute.edu.vn.HeThongQuanLyRapPhim.model.TrangThaiPhim;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.Set;
 
 public interface MovieRepository extends JpaRepository<Phim, Integer> {
+    // Tìm theo trạng thái phim
     Set<Phim> findByTrangThaiPhim(TrangThaiPhim trangThaiPhim);
+
+    // Tìm theo tên phim
+    Optional<Phim> findByTenPhim(String tenPhim);
 }
