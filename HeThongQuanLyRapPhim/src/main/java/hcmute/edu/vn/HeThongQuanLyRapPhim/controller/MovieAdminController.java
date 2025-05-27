3d79777d508a8f46295bdf805ed9f5a8a152517b
bigbo <22110177@student.hcmute.edu.vn>
2025-05-27 11:18:02 +0700

    Cải tiến code

--- HeThongQuanLyRapPhim/src/main/java/hcmute/edu/vn/HeThongQuanLyRapPhim/controller/MovieAdminController.java
+++ HeThongQuanLyRapPhim/src/main/java/hcmute/edu/vn/HeThongQuanLyRapPhim/controller/MovieAdminController.java
@@ -5,11 +5,9 @@ import hcmute.edu.vn.HeThongQuanLyRapPhim.decorator.pagination.PaginatedMovieSer
 import hcmute.edu.vn.HeThongQuanLyRapPhim.decorator.sort.SortedMovieService;
 import hcmute.edu.vn.HeThongQuanLyRapPhim.model.*;
 import hcmute.edu.vn.HeThongQuanLyRapPhim.service.MovieService;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
