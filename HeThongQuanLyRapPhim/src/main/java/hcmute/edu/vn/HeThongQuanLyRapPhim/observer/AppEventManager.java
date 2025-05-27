5a1c25069217118758db707f2411f5bb5cfa9b1f
Lục Thới Trọng <lucthoitrong@gmail.com>
2025-05-27 07:47:30 +0700

    Điều chỉnh mẫu observer

--- HeThongQuanLyRapPhim/src/main/java/hcmute/edu/vn/HeThongQuanLyRapPhim/observer/AppEventManager.java
+++ HeThongQuanLyRapPhim/src/main/java/hcmute/edu/vn/HeThongQuanLyRapPhim/observer/AppEventManager.java
@@ -1,6 +1,7 @@
 package hcmute.edu.vn.HeThongQuanLyRapPhim.observer;
 
 import hcmute.edu.vn.HeThongQuanLyRapPhim.event.AppEvent;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -10,21 +11,28 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
+@Component
 public class AppEventManager implements Subject {
+//  1 sự kiện -> 1 list xử lý sự kiện
     private final Map<Class<? extends AppEvent>, List<EventListener>> listenersPerEventType = new HashMap<>();
+
+//  Biến để xử lý đa luồng
     private final ExecutorService notificationExecutor;
 
     public AppEventManager() {
         this.notificationExecutor = Executors.newCachedThreadPool();
     }
 
+    @Override
     public void subscribe(Class<? extends AppEvent> eventType, EventListener listener) {
+//      computeIfAbsent: Nếu đã có -> dùng lại danh sách cũ, ngược lại tạo mới 1 danh sách
         List<EventListener> listeners = listenersPerEventType.computeIfAbsent(eventType, k -> new ArrayList<>());
         if (!listeners.contains(listener)) {
             listeners.add(listener);
         }
     }
 
+    @Override
     public void unsubscribe(Class<? extends AppEvent> eventType, EventListener listener) {
         List<EventListener> listeners = listenersPerEventType.get(eventType);
         if (listeners != null) {
@@ -35,11 +43,14 @@ public class AppEventManager implements Subject {
         }
     }
 
+    @Override
     public void notify(AppEvent event) {
         if (event == null) {
             return;
         }
+//      Lấy loại sự kiện
         Class<? extends AppEvent> eventType = event.getClass();
+//      Lấy danh sách xử lý sự kiện từ loại sự kiện đó
         List<EventListener> specificListeners = listenersPerEventType.get(eventType);
 
         if (specificListeners != null && !specificListeners.isEmpty()) {
@@ -49,13 +60,13 @@ public class AppEventManager implements Subject {
                         listener.update(event);
                     } catch (Exception e) {
                         System.err.println("Lỗi khi listener " + listener.getClass().getName() + " xử lý sự kiện " + eventType.getName() + ": " + e.getMessage());
-                        // Trong ứng dụng thực tế, nên dùng logger
                     }
                 });
             }
         }
     }
 
+    @Override
     public void shutdown() {
         notificationExecutor.shutdown();
         try {
